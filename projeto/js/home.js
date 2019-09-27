e10064f746cb9880db45b47513d28e1ef41ac13d
unknown <wescley188@gmail.com>
2019-09-26 23:14:57 -0300

    Layouts (Home-Impressora3d-CorteALazer-Noticias-NoticiasAbertas) 95% finalizados

--- projeto/js/home.js
+++ projeto/js/home.js
@@ -6,28 +6,3 @@
             behavior: 'smooth' // 3
         });
     }
-        
-        
-        const target = document.querySelectorAll('[data-anime]');
-        const animationClass = 'animate';
-
-        // https://www.youtube.com/watch?v=pKTOT63X9XQ
-        function animeScroll() {
-            const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
-            target.forEach(function (element) {
-                if ((windowTop) > element.offsetTop) {
-                    element.classList.add(animationClass);
-                } else {
-                    element.classList.remove(animationClass);
-                }
-
-            })
-        }
-
-        animeScroll();
-
-        if (target.length) {
-            window.addEventListener('scroll', function () {
-                animeScroll();
-            })
-        }
